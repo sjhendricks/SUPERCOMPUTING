4f5ab9e4cd0ace2d3bdd8e8edbc1a56a879ce2ec
Hendricks_ Sarah <sjhendricks@bora.sciclone.wm.edu>
2026-02-25 12:15:19 -0500

    Summarize_fasta script work

--- assignments/assignment_04/scripts/summarize_fasta.sh
+++ assignments/assignment_04/scripts/summarize_fasta.sh
@@ -2,10 +2,19 @@
 set -ueo pipefail
 
 # save fasta file name as a variable (input)
-file=$1
+file=${1}
+# shorten file name
+short=$(basename $file)
 # calculate and save total number of sequences
-seqs=(grep "^>" file | wc -l)
+seqs=$(grep "^>" ${file} | wc -l)
 # calculate and save total number of nucleotides
-nucleotides=(grep -v "^>" file | wc -c)
+nucleotides=$(grep -v "^>" ${file} |tr -d "\n" | wc -c)
 # table of sequence names and lengths
+table=$(seqtk comp ${file} | cut -f1,2)
 
+echo "Total Number of Sequences in ${short}"
+echo "${seqs}"
+echo "Total Number of Nucleotides in ${short}:"
+echo "${nucleotides}"
+echo "All Sequence names and lengths:"
+echo "${table}"
